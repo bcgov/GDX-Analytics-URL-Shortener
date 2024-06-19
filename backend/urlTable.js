bd6a7be3a07b9dc9dc9698881a4fb183b3482631
Ozdemir Ozcelik <ozdemir.ozcelik@gov.bc.ca>
2024-06-19 15:27:56 -0700

    Sorted the results by createdTime

--- backend/urlTable.js
+++ backend/urlTable.js
@@ -2,8 +2,8 @@ import { UrlModel } from './db.js';
 
 export const getUrlTable = async (req, res, next) => {
   try {
-    // Retrieve the list of URLs from the database
-    const urlTable = await UrlModel.find();
+    // Retrieve the list of URLs from the database and sort by 'createdTime'
+    const urlTable = await UrlModel.find().sort({ createdTime:-1 }); // 1 for ascending, -1 for descending
     res.json(urlTable);
   } catch (error) {
     console.error('Error retrieving URL table:', error);
