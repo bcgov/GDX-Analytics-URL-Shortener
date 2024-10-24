3762a8bd8aaa669bc1e6b109d3f1aebe5d2eeed5
Veenu Punyani <Veenu.veenu@gov.bc.ca>
2024-10-23 17:33:16 -0700

    added copy buttons and a short url

--- frontend/url-shortener/src/views/UrlSummary.vue
+++ frontend/url-shortener/src/views/UrlSummary.vue
@@ -1,107 +1,180 @@
 <template>
-  <div>
-    <h1>URL Summary</h1>
-    <p>Link #: {{ customId }}</p>
-    <p>Short URL: {{ shortenedUrlString }}</p>
-    <p>Target URL: {{ targetUrl }}</p>
-    <p>Notes: {{ description }}</p>
-    <p>Expiry Date: {{ expiryDate }}</p>
-    <!-- Add the following lines for tags, created by, and created time 
-    <p>Tags: {{ tags }}</p>-->
-    <p>Created By: {{ createdBy }}</p>
-    <p>Created On: {{ createdTime }}</p>
-    <!-- Add a router-link to navigate to the URL List page -->
-    <router-link :to="{ name: 'url-table' }">Check all URLs</router-link>
+  <div class="url-summary-container">
+    <div class="url-details">
+      <p>
+        <strong>Short URL:</strong>
+        <a :href="shortenedUrl" target="_blank" class="short-url">{{ shortenedUrl }}</a>
+        <a href="#" class="copy-btn" @click="copyToClipboard(shortenedUrl)">
+            <img src="../assets/copy.svg" alt="Copy icon">
+          </a>
+          
+      </p>
+      <h3>Details</h3>
+      <p>
+        <strong>Full Short URL:</strong>
+        <a :href="shortenedUrl" target="_blank" class="short-url">{{ shortenedUrl }}</a>
+        <a href="#" class="copy-btn" @click="copyToClipboard(shortenedUrl)">
+            <img src="../assets/copy.svg" alt="Copy icon">
+          </a>
+          
+      </p>
+      <p>
+        <strong>Target URL:</strong>
+        <a :href="targetUrl" target="_blank" class="short-url">{{ targetUrl }}</a>
+        <a href="#" class="copy-btn" @click="copyToClipboard(targetUrl)">
+            <img src="../assets/copy.svg" alt="Copy icon">
+          </a>
+          
+      </p>
+      <p>
+        <strong>Internal Link:</strong> 
+        <a :href="`${frontendURL}/url-summary/${customId}`" target="_blank">{{ `${frontendURL}/url-summary/${customId}` }}</a>
+        <a href="#" class="copy-btn" @click="copyToClipboard(`${frontendURL}/url-summary/${customId}`)">
+            <img src="../assets/copy.svg" alt="Copy icon">
+          </a>
+          
+      </p>
+      <p><strong>Expiry Date:</strong> {{ expiryDate }}</p>
+      <p><strong>Created By:</strong> {{ createdBy }}</p>
+      <p><strong>Created Date/Time:</strong> {{ createdTime }}</p>
+      <p><strong>Edited Date/Time:</strong> </p>
+      <p><strong>Notes:</strong> {{ description }}</p>
+      <p style="color: green;">{{ copiedMessage }}</p>
+    </div>
+
+    <div class="action-container">
+      <router-link :to="{ name: 'url-table' }" class="url-list-link">Check all URLs</router-link>
+    </div>
   </div>
 </template>
 
+
 <script setup>
 import { ref, onMounted } from 'vue';
-import { useRoute } from 'vue-router'; // Import useRoute
+import { useRoute } from 'vue-router';
 import axios from 'axios';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 
-const route = useRoute(); // Use useRoute to get access to $route
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const route = useRoute();
 const customId = ref('');
-const shortenedUrlString = ref('');
+const shortenedUrl = ref('');
 const targetUrl = ref('');
 const description = ref('');
 const expiryDate = ref('');
-// const tags = ref('');
-const createdBy = ref('');
 const createdTime = ref('');
-const frontendURL = import.meta.env.VITE_FRONTEND_URL;
+const createdBy = ref('');
 const backendURL = import.meta.env.VITE_BACKEND_URL;
-// Import the dayjs library for date manipulation.
-// Day.js is a minimalist JavaScript library that parses, validates, manipulates, and displays dates and times for modern browsers. Source - https://day.js.org/
-import dayjs from 'dayjs';
-
-// Import the UTC plugin to handle UTC times
-import utc from 'dayjs/plugin/utc';
-
-// Import the timezone plugin to handle timezone conversions
-import timezone from 'dayjs/plugin/timezone';
-
-// Extend dayjs with the UTC plugin for working with UTC dates
-dayjs.extend(utc);
-
-// Extend dayjs with the timezone plugin to allow timezone conversions
-dayjs.extend(timezone);
-
+const frontendURL = import.meta.env.VITE_FRONTEND_URL;
+const copiedMessage = ref('');
 
 onMounted(async () => {
-  // Assign the customId value from the route parameters using useRoute
   customId.value = route.params.customId;
 
   try {
-    // Retrieve data from the backend using customId
     const response = await axios.get(`${backendURL}/url-summary/${customId.value}`);
-
     const data = response.data;
 
-    // Update data properties based on the retrieved data
-    shortenedUrlString.value = data.shortenedUrlString;
+    shortenedUrl.value = data.shortenedUrl;
     targetUrl.value = data.targetUrl;
     description.value = data.description;
     expiryDate.value = data.expiryDate;
+    customId.value = data.customId;
 
-    // Check if these properties exist in the response before updating
-    // if ('tags' in data) {
-    //   tags.value = data.tags;
-    // }
-
-    if ('createdBy' in data) {
-      createdBy.value = data.createdBy;
-    }
-
-    // Check if 'createdTime' property exists in the 'data' object
     if ('createdTime' in data) {
-    
-    // Convert 'createdTime' from UTC to local time using the convertToLocalTime function
-    // This function adjusts for the user's timezone and handles Daylight Savings Time (DST)
-      data.createdTime = convertToLocalTime(data.createdTime);
-    // Update the 'createdTime' reactive value to reflect the converted local time
-      createdTime.value = data.createdTime;
+      createdTime.value = convertToLocalTime(data.createdTime);
     }
 
   } catch (error) {
     console.error('Error retrieving URL details:', error);
-    // Handle the error gracefully, e.g., show a user-friendly message
   }
 });
 
-// Define a function that converts a given UTC date/time to local time
-// This conversion is aware of the user's local timezone and considers Daylight Savings Time (DST)
-// The function uses the 'dayjs' library along with the 'timezone' plugin
-
 const convertToLocalTime = (utcDate) => {
-  
-  // The 'tz.guess()' function automatically detects the user's current timezone
-  // The 'tz()' method then converts the given UTC date/time to the detected timezone
-  // The 'format()' method ensures the date is returned in a readable format: 'YYYY-MM-DD HH:mm:ss'
-  
-  return dayjs(utcDate)  // Takes the UTC date/time passed to the function
-    .tz(dayjs.tz.guess())  // Converts it to the user's local timezone (with DST adjustments)
-    .format('YYYY-MM-DD HH:mm:ss');  // Formats the result in 'YYYY-MM-DD HH:mm:ss' format
+  return dayjs(utcDate)
+    .tz(dayjs.tz.guess())
+    .format('YYYY-MM-DD HH:mm:ss');
+};
+
+
+const copyToClipboard = (text) => {
+  const input = document.createElement('textarea');
+  input.value = text;
+  document.body.appendChild(input);
+  input.select();
+  document.execCommand('copy');
+  document.body.removeChild(input);
+  copiedMessage.value = 'URL copied!';
+  setTimeout(() => {
+    copiedMessage.value = '';
+  }, 5000); // Hide the message after 5 seconds
 };
 
 </script>
+
+
+
+<style scoped>
+.url-summary-container {
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.url-details {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 15px;
+  font-size: 1.1em;
+  color: #333;
+}
+
+.url-details p {
+  margin: 5px 0;
+}
+
+.short-url {
+  color: #0056b3;
+  font-weight: bold;
+  word-break: break-word;
+  text-decoration: underline;
+}
+
+.copy-btn {
+  display: inline-block;
+  width: 18px;
+  height: 18px;
+  background-image: url('../assets/copy.svg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  cursor: pointer;
+  margin-left: 10px;
+}
+
+.copy-btn:hover {
+  background-color: #0056b3;
+}
+
+.target-url-container {
+  word-break: break-word;
+  overflow-wrap: break-word;
+}
+
+.action-container {
+  margin-top: 20px;
+  text-align: center;
+}
+
+.url-list-link {
+  color: #0056b3;
+  text-decoration: underline;
+}
+
+.url-list-link:hover {
+  color: #003f88;
+}
+</style>
